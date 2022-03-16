f0d8477222136974f502565e00b4a54ccb7f8084
student <student@B420-24>
2022-03-16 17:34:40 +0100

    Initial commit

---
+++ PIS_lab1.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PIS_lab1.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
