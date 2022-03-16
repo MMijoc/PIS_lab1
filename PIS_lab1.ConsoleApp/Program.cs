b5ab4376213283b44147eb452c190d802608066f
mmaret00 <73230708+mmaret00@users.noreply.github.com>
2022-03-16 17:45:18 +0100

    komentar
    dodavanje komentara

--- PIS_lab1.ConsoleApp/Program.cs
+++ PIS_lab1.ConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace PIS_lab1.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");//komentar
+            Console.WriteLine("Hello World!");/#komentar
         }
     }
 }
